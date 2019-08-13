3a26e3891f9e1b7592b720081842d42bb63fa517
121 <shoogoome@sina.com>
2019-08-13 12:25:43 +0800

    update

--- devops/init.sh
+++ devops/init.sh
@@ -8,10 +8,9 @@ init_path() {
     mkdir -p ${WORK_DIR}'/redis'
     mkdir -p ${WORK_DIR}'/mongo'
     for i in `seq 0 5`; do
-        mkdir -p ${WORK_DIR}'/server/${i}/root'
-        mkdir -p ${WORK_DIR}'/server/${i}/tmp'
-        mkdir -p ${WORK_DIR}'/server/${i}/logs'
+        mkdir -p ${WORK_DIR}'/server/'${i}'/root'
+        mkdir -p ${WORK_DIR}'/server/'${i}'/tmp'
+        mkdir -p ${WORK_DIR}'/server/'${i}'/logs'
     done
 }
-
 init_path
