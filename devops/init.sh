ef9df48d91b3f653c3239f7dd09657d767647095
121 <shoogoome@sina.com>
2019-08-13 12:14:59 +0800

    update

---
+++ devops/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ..
+WORK_DIR=`pwd`
+
+# 初始化文件路径
+init_path() {
+    mkdir -p ${WORK_DIR}'/redis'
+    mkdir -p ${WORK_DIR}'/mongo'
+    for i in `seq 0 5`; do
+        mkdir -p ${WORK_DIR}'/server/root'
+        mkdir -p ${WORK_DIR}'/server/tmp'
+        mkdir -p ${WORK_DIR}'/server/logs'
+    done
+}
